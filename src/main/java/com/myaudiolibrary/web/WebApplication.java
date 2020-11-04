502d43053abfd5c7259adac50fd85b6c056fdf54
bouch mana <mana99hotmail.fr>
2020-11-04 15:09:28 +0100

    initial commit

---
+++ src/main/java/com/myaudiolibrary/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.myaudiolibrary.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+//mettre ces entites ici
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
